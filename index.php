d183749224897ad0a0045e6a756ebdda7e6c0979
Heitor Chang <heitorchang@gmail.com>
2018-08-03 21:40:29 -0300

    session_start

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ include("header.php");
 
 <h1>Cabra</h1>
 
-<?php
-session_start();
 
 // check for login session or cookie, if set, display welcome back
 // otherwise display a login form and cadastro link
